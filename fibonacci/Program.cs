9bc33d79422031f5a03d56fa02bbbb37f106a1db
jyhdez1212 <35971230+jyhdez1212@users.noreply.github.com>
2020-09-15 23:55:11 -0500

    Add project files.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace fibonacci
+{
+	class Program
+	{
+		public static int FibonacciSequence(int n)
+		{
+			int x = 0;
+			int y = 1;
+			
+			for (int i = 0; i < n; i++)
+			{
+				int temp = x;
+				x = y;
+				y = temp + y;
+			}
+			return x;
+
+		}
+		static void Main()
+		{
+			Console.Write("\n\n\n Input sequence number: ");
+			int s = Convert.ToInt32(Console.ReadLine());
+			Console.Write("\n\n The Fibonacci sequence number at {0} is : ", s);
+			
+			
+			Console.Write("{0} ", FibonacciSequence(s-1));
+			
+			
+
+		}
+	}
+}
